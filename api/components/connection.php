b0d45dd3b2edf07dd220adf2781bfebe19217ac7
Tristan <tristanbudd@outlook.com>
2025-10-02 07:34:44 +0100

    Attempt Fix routing

--- api/components/connection.php
+++ api/components/connection.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SERVER["DOCUMENT_ROOT"] . "/api/vendor/autoload.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php");
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/starwars-website/');
 $dotenv->load();
